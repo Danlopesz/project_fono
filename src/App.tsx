d90701ca04bbb19b5a8d143cb6e3c0d810d6b79f
Daniel Almeida Lopes <116038774+Danlopesz@users.noreply.github.com>
2026-04-17 18:19:29 -0300

    refactor: update vercel.json to use rewrites instead of routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import SalaProfessores from "./pages/SalaProfessores";
-import Banheiro from "./pages/Banheiro";
-import Refeitorio from "./pages/Refeitorio";
-import Elevador from "./pages/Elevador";
+import Index from "@/pages/Index";
+import SalaProfessores from "@/pages/SalaProfessores";
+import Banheiro from "@/pages/Banheiro";
+import Refeitorio from "@/pages/Refeitorio";
+import Elevador from "@/pages/Elevador";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/sala-professores" element={<SalaProfessores />} />
-        <Route path="/banheiro" element={<Banheiro />} />
-        <Route path="/refeitorio" element={<Refeitorio />} />
-        <Route path="/elevador" element={<Elevador />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/sala-professores" element={<SalaProfessores />} />
+                <Route path="/banheiro" element={<Banheiro />} />
+                <Route path="/refeitorio" element={<Refeitorio />} />
+                <Route path="/elevador" element={<Elevador />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
